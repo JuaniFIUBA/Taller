4ef9fc5af7bf931110f8e56f2b112d09f2f6af7a
JuaniFIUBA <jperezd@fi.uba.ar>
2023-09-28 10:16:49 -0300

    a

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 use tp_individual::explosion::Explosion;
 use tp_individual::io;
 use tp_individual::mapa::Mapa;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (file_path_origen, file_path_destino, x, y) = io::obtener_input()?;
     let mut mapa = match Mapa::  crear_mapa(&file_path_origen)
-        .map_err(|err| format!("Error al crear el mapa. {}", err))
     {
         Ok(mapa) => mapa,
         Err(err) => {
-            io::guardar_error_y_salir(&err, &file_path_destino)?;
+            io::guardar_error_y_salir(&err.to_string(), &file_path_destino)?;
             return Err(Box::new(std::io::Error::new(
                 std::io::ErrorKind::Other,
-                err,
+                err.to_string(),
             )));
         }
     };
